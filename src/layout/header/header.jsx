4d9b2d7be55b7f37a0aacad02e5955cb035f4a9f
mehriddin-amonboyev <mehriddinamonboyev1@gmail.com>
2025-02-13 19:25:56 +0500

    first commit

---
+++ src/layout/header/header.jsx
@@ -0,0 +1,19 @@
+import "./header.css";
+import headerLogo from "../../assets/svg/liberIcon.svg"
+export const Header = () => {
+    return
+    <section className="header">
+        <div className="container">
+            <div className="header__block">
+                <div className="header__logo">
+                    <img src={headerLogo} alt="header logotipi" />
+                </div>
+                
+                <div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+};
